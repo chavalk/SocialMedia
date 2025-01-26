d0120b889b6b5dbd850a6b22717dda852125d9a1
Jose Garcia <chavalk@hotmail.com>
2025-01-25 22:41:29 -0600

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by Jose Garcia on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
